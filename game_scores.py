c0944442ca6fe5251e9f4f595dd37606fc425c7a
bjorn Johnsson <u0798467@umail.utah.edu>
2024-05-10 11:17:18 -0600

    new file:   __pycache__/player_A.cpython-39.pyc 	new file:   __pycache__/round_scores1.cpython-39.pyc 	deleted:    current_total.py 	new file:   game_scores.py 	new file:   round_scores1.py

---
+++ game_scores.py
@@ -0,0 +1,17 @@
+from round_scores1 import round_scores
+import numpy as np
+#test_round=round_scores()
+number_of_players=1
+total_score=np.zeros((number_of_players,1))
+for round in range(30):
+    current_round_score=round_scores()
+    #print("for round number", round, "the scores were", current_round_score)
+    for player in range(number_of_players):
+        total_score[player]=total_score[player]+current_round_score[player]
+print("the total scores were",total_score)
+winner=np.argmax(total_score)
+print("the winner is player", winner+1)
+if winner==0:
+    print('the winning strat was taking anything over 21')
+
+    
---
+++ round_scores1.py
@@ -0,0 +1,59 @@
+#uses roller to get the current total
+
+from roller import roll_dice
+from player_A import player_A_strat
+import numpy as np
+def round_scores():
+    total=0
+    current_roll=2
+    player_A_in=1
+    all_players=[player_A_in]
+    all_players_round_score=np.zeros((1,1))
+
+    while sum(all_players)>.1:
+        #decideds to roll dice or will end round
+        current_roll=roll_dice()
+        #print('the current roll is')
+        #print(current_roll)
+        #rolls one dice form 1 to 6
+        if current_roll>2:
+            #adds current roll to total if it rolls 3 through 6
+            total=total+current_roll
+        elif current_roll == 2:
+            # doubles total if it rolls a 2
+            if total==0:
+                total=4
+            else:
+                total=total*2
+                
+        else:
+            # ends round if a 1 is rolled
+            #print('round over rolled a 1')
+            total=0
+                        
+            for i in range(len(all_players)):
+                #scores 0 for all players who havent scored yet for the round
+                if all_players[i]==1:
+                    all_players_round_score[i]=0
+                    all_players[i]=0
+            #print(all_players)
+            break
+
+        #print(total)
+        # checks for scoring
+        if player_A_in ==1:
+            #if current player is still in checks to see if they are still in after roll
+            player_A_in=player_A_strat(total)
+            if player_A_in == 0:
+                all_players_round_score[0]=total
+                #print('player a has scored')
+
+        #update number of players
+        all_players=[player_A_in]
+        
+
+    #print('the round is over')
+
+
+    #print(all_players_round_score)
+    return(all_players_round_score)
