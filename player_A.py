aed50fa957692254ad6afd4c8720cd8aedc5873c
bjorn Johnsson <u0798467@umail.utah.edu>
2024-05-10 10:19:58 -0600

    new file:   __pycache__/roller.cpython-39.pyc 	modified:   current_total.py 	new file:   player_A.py

---
+++ player_A.py
@@ -0,0 +1,9 @@
+#status of player A 
+
+def player_A_strat(total):
+    if total>20:
+        #means player a scores for that round
+        return(0)
+    else:
+        #means player A is still going
+        return(1)
